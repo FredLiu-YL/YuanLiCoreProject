f47e4a4e323aa1b85905974f7207bc636f9f3084
zhengye_lin@yuanyu.tw <zhengye_lin@yuanyu.tw>
2023-10-30 10:46:00 +0800

    新增ProductionUI頁面，Listbox_新增Button許多Type

---
+++ YuanliCore.Model/UserControls/Information/CassetteUC.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace YuanliCore.Model.Information
+{
+    /// <summary>
+    /// CassetteUC.xaml 的互動邏輯
+    /// </summary>
+    /// 
+    public partial class CassetteUC : UserControl, INotifyPropertyChanged
+    {
+
+        private SolidColorBrush PrimaryHueLightBrush = Application.Current.Resources["PrimaryHueLightBrush"] as SolidColorBrush;
+
+        //Color blueGreyColor2 = (Color)Application.Current.FindResource(new Uri("pack://application:,,,/MaterialDesignColors;component/Themes/Recommended/Primary/MaterialDesignColor.bluegrey.xaml", UriKind.RelativeOrAbsolute));
+
+
+        //Color blueGreyColor = (Color)Application.Current.FindResource("PrimaryHueMidBrush");
+
+
+        private bool btn1_IsClik = true;
+        public bool Btn1_IsClik { get => btn1_IsClik; set => SetValue(ref btn1_IsClik, value); }
+
+        private bool btn2_IsClik = true;
+        public bool Btn2_IsClik { get => btn2_IsClik; set => SetValue(ref btn2_IsClik, value); }
+
+        private bool btn3_IsClik = true;
+        public bool Btn3_IsClik { get => btn3_IsClik; set => SetValue(ref btn3_IsClik, value); }
+
+
+        private Brush btn1_Background;
+        public Brush Btn1_Background { get => btn1_Background; set => SetValue(ref btn1_Background, value); }
+
+        private Brush btn2_Background;
+        public Brush Btn2_Background { get => btn2_Background; set => SetValue(ref btn2_Background, value); }
+
+        private Brush btn3_Background;
+        public Brush Btn3_Background { get => btn3_Background; set => SetValue(ref btn3_Background, value); }
+
+        public Brush Click_On = Brushes.Red;
+
+        public Brush Click_Off = Brushes.Yellow;
+
+        public CassetteUC()
+        {
+            InitializeComponent();
+        }
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ResourceDictionary dictionary = new ResourceDictionary();
+                dictionary.Source = new Uri("pack://application:,,,/MaterialDesignColors;component/Themes/Recommended/Primary/MaterialDesignColor.bluegrey.xaml", UriKind.RelativeOrAbsolute);
+
+                if (dictionary.Count > 0)
+                {
+                    // 资源字典已加载
+                    object blueGreyColorResource1 = dictionary["PrimaryHueLightBrush"];
+                    if (blueGreyColorResource1 != null && blueGreyColorResource1 is SolidColorBrush)
+                    {
+                        SolidColorBrush brush = (SolidColorBrush)blueGreyColorResource1;
+                        Color blueGreyColor = brush.Color;
+                        Click_Off = brush;
+                        // 现在你可以使用 blueGreyColor
+                    }
+                    // 资源字典已加载
+                    object blueGreyColorResource2 = dictionary["PrimaryHueDarkBrush"]; //PrimaryHueDarkForegroundBrush
+                    if (blueGreyColorResource2 != null && blueGreyColorResource2 is SolidColorBrush)
+                    {
+                        SolidColorBrush brush = (SolidColorBrush)blueGreyColorResource2;
+                        Color blueGreyColor = brush.Color;
+                        Click_On = brush;
+                        // 现在你可以使用 blueGreyColor
+                    }
+                }
+
+                Btn1_Background = Click_Off;
+                Btn2_Background = Click_Off;
+                Btn3_Background = Click_Off;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public ICommand Btn1_Command => new RelayCommand(async () =>
+        {
+            try
+            {
+                if (Btn1_IsClik == true)
+                {
+                    Btn1_IsClik = false;
+                    Btn1_Background = Click_Off;
+                }
+                else
+                {
+                    Btn1_IsClik = true;
+                    Btn1_Background = Click_On;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        });
+        public ICommand Btn2_Command => new RelayCommand(async () =>
+        {
+            try
+            {
+                if (Btn2_IsClik == true)
+                {
+                    Btn2_IsClik = false;
+                    Btn2_Background = Click_Off;
+                }
+                else
+                {
+                    Btn2_IsClik = true;
+                    Btn2_Background = Click_On;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        });
+        public ICommand Btn3_Command => new RelayCommand(async () =>
+        {
+            try
+            {
+                if (Btn3_IsClik == true)
+                {
+                    Btn3_IsClik = false;
+                    Btn3_Background = Click_Off;
+                }
+                else
+                {
+                    Btn3_IsClik = true;
+                    Btn3_Background = Click_On;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        });
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            T oldValue = field;
+            field = value;
+            OnPropertyChanged(propertyName, oldValue, value);
+        }
+
+        protected virtual void OnPropertyChanged<T>(string name, T oldValue, T newValue)
+        {
+            // oldValue 和 newValue 目前沒有用到，代爾後需要再實作。
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ YuanliCore.Model/UserControls/Information/ProductionUC.xaml.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace YuanliCore.Model.Information
+{
+    /// <summary>
+    /// Informaiton.xaml 的互動邏輯
+    /// </summary>
+    public partial class ProductionUC : UserControl, INotifyPropertyChanged
+    {
+        //private ListBox topListBoxItem;
+        //public ListBox TopListBoxItem { get => topListBoxItem; set => SetValue(ref topListBoxItem, value); }
+
+        private ObservableCollection<ObservableCollection<Button>> topListBoxItemSource = new ObservableCollection<ObservableCollection<Button>>();
+        public ObservableCollection<ObservableCollection<Button>> TopListBoxItemSource { get => topListBoxItemSource; set => SetValue(ref topListBoxItemSource, value); }
+
+
+        private ObservableCollection<Button> outerListBox = new ObservableCollection<Button>();
+        public ObservableCollection<Button> OuterListBox { get => outerListBox; set => SetValue(ref outerListBox, value); }
+        //    <ListBox.ItemTemplate>
+        //        <DataTemplate>
+        //            <Button Content = "{Binding}" />
+        //        </ DataTemplate >
+        //    </ ListBox.ItemTemplate >
+
+        private ObservableCollection<ObservableCollection<string>> buttonRows = new ObservableCollection<ObservableCollection<string>>();
+        public ObservableCollection<ObservableCollection<string>> ButtonRows { get => buttonRows; set => SetValue(ref buttonRows, value); }
+
+        private ObservableCollection<ObservableCollection<ButtonState>> buttonRows_New = new ObservableCollection<ObservableCollection<ButtonState>>();
+        public ObservableCollection<ObservableCollection<ButtonState>> ButtonRows_New { get => buttonRows_New; set => SetValue(ref buttonRows_New, value); }
+
+
+
+
+        //CassetteUC
+        public class ButtonState
+        {
+            public string Context { get; set; }
+            public bool IsClick { get; set; }
+        }
+
+        public ProductionUC()
+        {
+            InitializeComponent();
+        }
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddButtonAction = new RelayCommand<(int, int)>(key =>
+                {
+                    int variable1 = key.Item1;
+                    int variable2 = key.Item2;
+                    AddButton(variable1, variable2);
+                });
+                CassetteUC = new ObservableCollection<CassetteUC>();
+                AddButton(3, 25);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+
+
+
+
+
+        //public static readonly DependencyProperty AddShapeActionProperty = DependencyProperty.Register(nameof(AddShapeAction), typeof(ICommand), typeof(ProductionUC),
+        //                                                                            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty AutoSaveIsCheckedProperty = DependencyProperty.Register(nameof(AutoSaveIsChecked), typeof(bool), typeof(ProductionUC),
+                                                                                     new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty CircleIsCheckedProperty = DependencyProperty.Register(nameof(CircleIsChecked), typeof(bool), typeof(ProductionUC),
+                                                                                     new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty DieInsideAllCheckedProperty = DependencyProperty.Register(nameof(DieInsideAllChecked), typeof(bool), typeof(ProductionUC),
+                                                                                     new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty AddButtonActionProperty = DependencyProperty.Register(nameof(AddButtonAction), typeof(ICommand), typeof(ProductionUC),
+                                                                                     new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+
+        public bool AutoSaveIsChecked
+        {
+            get => (bool)GetValue(AutoSaveIsCheckedProperty);
+            set => SetValue(AutoSaveIsCheckedProperty, value);
+        }
+        public bool CircleIsChecked
+        {
+            get => (bool)GetValue(CircleIsCheckedProperty);
+            set => SetValue(CircleIsCheckedProperty, value);
+        }
+        public bool DieInsideAllChecked
+        {
+            get => (bool)GetValue(DieInsideAllCheckedProperty);
+            set => SetValue(DieInsideAllCheckedProperty, value);
+        }
+        public ICommand AddButtonAction
+        {
+            get => (ICommand)GetValue(AddButtonActionProperty);
+            set => SetValue(AddButtonActionProperty, value);
+        }
+
+
+
+        //private ObservableCollection<CassetteUC> cassetteUC = new ObservableCollection<CassetteUC>();
+        //public ObservableCollection<CassetteUC> CassetteUC { get => cassetteUC; set => SetValue(ref cassetteUC, value); }
+
+        //--------------------------------
+
+        //public static readonly DependencyProperty CassetteUC2Property = DependencyProperty.Register(nameof(CassetteUC2), typeof(CassetteUC), typeof(ProductionUC),
+        //                                                                       new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        //public CassetteUC CassetteUC2
+        //{
+        //    get => (CassetteUC)GetValue(CassetteUC2Property);
+        //    set => SetValue(CassetteUC2Property, value);
+        //}
+
+        //--------------------------------
+
+        public static readonly DependencyProperty CassetteUCProperty = DependencyProperty.Register(nameof(CassetteUC), typeof(ObservableCollection<CassetteUC>), typeof(ProductionUC),
+                                                                                                     new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));//new PropertyMetadata(null));
+                                                                                                     
+        public ObservableCollection<CassetteUC> CassetteUC
+        {
+            get => (ObservableCollection<CassetteUC>)GetValue(CassetteUCProperty);
+            set { SetValue(CassetteUCProperty, value); }
+        }
+
+        public void AddButton(int Cols, int Rows)
+        {
+            try
+            {
+                ButtonRows.Clear();
+                TopListBoxItemSource.Clear();
+                ButtonRows_New.Clear();
+                OuterListBox.Clear();
+                for (int i = 1; i <= Rows; i++)
+                {
+                    var row = new ObservableCollection<string>();
+                    var row2 = new ObservableCollection<Button>();
+                    var row3 = new ObservableCollection<ButtonState>();
+                    for (int j = 1; j <= Cols; j++)
+                    {
+                        row.Add("Button " + (i * 3 - 3 + j));
+                        Button newButton = new Button();
+                        newButton.Content = "Button " + (i * 3 - 3 + j);
+                        newButton.Command = ListBoxButton_Command2;
+                        newButton.Tag = (i * 3 - 3 + j); // 將內容存儲在Tag屬性中
+                        newButton.Width = 50;
+                        newButton.Height = 20;
+                        newButton.Background = Brushes.Black;
+                        //newButton.Click += ListBoxButton_Click2;
+                        OuterListBox.Add(newButton);
+                        row2.Add(newButton);
+                        row3.Add(new ButtonState { Context = "Button " + (i * 3 - 3 + j), IsClick = false });
+                    }
+                    CassetteUC.Add(new CassetteUC { Btn1_IsClik = false, Btn2_IsClik = false, Btn3_IsClik = false });
+
+                    ButtonRows.Add(row);
+                    TopListBoxItemSource.Add(row2);
+                    ButtonRows_New.Add(row3);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public ICommand ListBoxButtonCommand => new RelayCommand(() =>
+        {
+            //if (parameter != null)
+            //{
+            //    // parameter 包含按钮的数据，可以根据需要进行处理
+            //    // 例如，你可以访问Button的Tag属性来识别按钮
+            //    //var buttonData = parameter as YourButtonDataClass;
+            //}
+        });
+
+
+
+        private void ListBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Click="ListBoxButton_Click"
+            try
+            {
+                Button clickedButton = sender as Button;
+
+                if (clickedButton != null)
+                {
+                    string buttonText = clickedButton.Tag as string;
+                    if (buttonText != null)
+                    {
+                        Brush now = clickedButton.Background;
+                        //MessageBox.Show("Button " + buttonText + "被點擊了！");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void ListBoxButton_Click2(object sender, RoutedEventArgs e)
+        {
+            // Click="ListBoxButton_Click"
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public ICommand ListBoxButton_Command2 => new RelayCommand(async () =>
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            T oldValue = field;
+            field = value;
+            OnPropertyChanged(propertyName, oldValue, value);
+        }
+
+        protected virtual void OnPropertyChanged<T>(string name, T oldValue, T newValue)
+        {
+            // oldValue 和 newValue 目前沒有用到，代爾後需要再實作。
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
