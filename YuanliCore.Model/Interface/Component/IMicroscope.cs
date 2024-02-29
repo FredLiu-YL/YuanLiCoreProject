9b94b7632605d9614f5b3a9b953120ea885ef1d5
zhengye_lin@yuanyu.tw <zhengye_lin@yuanyu.tw>
2024-02-29 16:46:08 +0800

    IMicroscope變更、BXFMUC更改畫面

--- YuanliCore.Model/Interface/Component/IMicroscope.cs
+++ YuanliCore.Model/Interface/Component/IMicroscope.cs
@@ -8,9 +8,135 @@ namespace YuanliCore.Model.Interface
 {
     public interface IMicroscope
     {
-
-
-
+        /// <summary>
+        /// 目前光強度
+        /// </summary>
+        int LightValue { get; set; }
+        /// <summary>
+        /// 目前光圈
+        /// </summary>
+        int ApertureValue { get; set; }
+        /// <summary>
+        /// 目前Z軸位置
+        /// </summary>
+        Task<double> GetZPosition();
+        /// <summary>
+        /// Z軟體負極限
+        /// </summary>
+        Task SetZNEL(int position);
+        /// <summary>
+        /// 取得Z軟體負極限
+        /// </summary>
+        Task<double> GetZNEL();
+        /// <summary>
+        /// Z軟體正極限
+        /// </summary>
+        Task SetZPEL(int position);
+        /// <summary>
+        /// 取得Z軟體正極限
+        /// </summary>
+        Task<double> GetZPEL();
+        /// <summary>
+        /// 準焦位置
+        /// </summary>
+        Task<double> GetAberationPosition();
+        /// <summary>
+        /// 取得準焦軟體負極限
+        /// </summary>
+        Task<int> GetAFNEL();
+        /// <summary>
+        /// 設定準焦軟體負極限
+        /// </summary>
+        Task SetAFNEL(int position);
+        /// <summary>
+        /// 取得準焦軟體正極限
+        /// </summary>
+        Task<int> GetAFPEL();
+        /// <summary>
+        /// 設定準焦軟體正極限
+        /// </summary>
+        Task SetAFPEL(int position);
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        void Initial();
+        /// <summary>
+        /// 復歸
+        /// </summary>
+        /// <returns></returns>
+        Task Home();
+        /// <summary>
+        /// 更換鏡頭 idx=1~5、6(有5、6孔)
+        /// </summary>
+        /// <param name="idx"></param>
+        Task ChangeLens(int idx);
+        /// <summary>
+        /// 更換鏡片組BF DF Other
+        /// </summary>
+        /// <param name="idx"></param>
+        Task ChangeCube(int idx);
+        /// <summary>
+        /// 更換光圈 ApertureValue=0~3113
+        /// </summary>
+        /// <param name="ApertureValue"></param>
+        /// <returns></returns>
+        Task ChangeAperture(int ApertureValue);
+        /// <summary>
+        /// 更換濾片，可裝1~3道濾片(wheelIdx)，每一到濾片可以切換1~6、7、8組(idx)
+        /// </summary>
+        /// <param name="wheelIdx">1~3</param>
+        /// <param name="idx">1~6、7、8</param>
+        /// <returns></returns>
+        Task ChangeFilter(int wheelIdx, int idx);
+        /// <summary>
+        /// 更換光亮度 LigntValue=0~120
+        /// </summary>
+        /// <param name="LigntValue"></param>
+        /// <returns></returns>
+        Task ChangeLight(int LigntValue);
+        /// <summary>
+        /// 更換光的發射方式 反射、透射
+        /// </summary>
+        /// <param name="idx"></param>
+        Task ChangeLightSpread(int idx);
+        /// <summary>
+        /// 一直對焦
+        /// </summary>
+        Task AF_Trace();
+        /// <summary>
+        /// 對焦一次
+        /// </summary>
+        Task AF_OneShot();
+        /// <summary>
+        /// 停止對焦
+        /// </summary>
+        Task AF_Off();
+        /// <summary>
+        /// 設定對焦的參數 FirstZPos(Z初始對焦位置) SearchRange(搜尋對焦的範圍)
+        /// </summary>
+        /// <param name="FirstZPos"></param>
+        /// <param name="SearchRange"></param>
+        Task SetSearchRange(double FirstZPos, double Range);
+        /// <summary>
+        /// 移動對焦軸相對位置
+        /// </summary>
+        /// <param name="distance"></param>
+        Task ZMoveCommand(double distance);
+        /// <summary>
+        /// 移動對焦軸絕對位置
+        /// </summary>
+        /// <param name="position"></param>
+        Task ZMoveToCommand(double position);
+        /// <summary>
+        /// 對焦值相對位置
+        /// </summary>
+        /// <param name="distance"></param>
+        Task AberrationMoveCommand(double distance);
+        /// <summary>
+        /// 對焦值絕對位置
+        /// </summary>
+        /// <param name="position"></param>
+        Task AberrationMoveToCommand(double position);
     }
 
 
--- YuanliCore.Model/UserControls/Microscope/BXFMUC.xaml.cs
+++ YuanliCore.Model/UserControls/Microscope/BXFMUC.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -25,22 +27,396 @@ namespace YuanliCore.Model.Microscope
     public partial class BXFMUC : UserControl, INotifyPropertyChanged
     {
 
-        private static readonly DependencyProperty MicroscopeProperty = DependencyProperty.Register(nameof(Microscope), typeof(IMicroscope), typeof(BXFMUC), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        private static readonly DependencyProperty MicroscopeProperty = DependencyProperty.Register(nameof(Microscope), typeof(IMicroscope), typeof(BXFMUC),
+                                                                                    new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public static readonly DependencyProperty BXFMUIShowProperty = DependencyProperty.Register(nameof(BXFMUIShow), typeof(BXFMUI), typeof(BXFMUC),
+                                                                                     new FrameworkPropertyMetadata(new BXFMUI(), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-     
         public BXFMUC()
         {
             InitializeComponent();
-         
+            //Loaded += UserControl_Loaded;
         }
-
         public IMicroscope Microscope
         {
             get => (IMicroscope)GetValue(MicroscopeProperty);
             set => SetValue(MicroscopeProperty, value);
         }
+        public BXFMUI BXFMUIShow
+        {
+            get => (BXFMUI)GetValue(BXFMUIShowProperty);
+            set => SetValue(BXFMUIShowProperty, value);
+        }
+
+        private bool isRefresh = false;
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                isRefresh = false;
+                RefreshStatus();
+            });
+        }
+        private async Task RefreshStatus()
+        {
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        if (isRefresh == true)
+                        {
+                            if (Microscope != null)
+                            {
+                                FocusZ = Convert.ToInt32(await Microscope.GetZPosition());
+
+                            }
+                            await Task.Delay(300);
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
 
 
+        private int intensitySliderValue;
+        private int distanceZ = 1000;
+        private bool isFocusZMove = true;
+        private int focusZ;
+        private int intensityValue;
+        private int apertureValue;
+        public int IntensitySliderValue
+        {
+            get
+            {
+                int returnValue = intensitySliderValue;
+                if (Microscope != null)
+                {
+                    returnValue = Microscope.LightValue;
+                }
+                return intensitySliderValue;
+            }
+            set
+            {
+                //Microscope.ChangeLight(value).Wait();
+
+                Microscope.LightValue = value;
+                SetValue(ref intensitySliderValue, value);
+                IntensityValue = value;
+            }
+        }
+        public int DistanceZ
+        {
+            get => distanceZ;
+            set
+            {
+                SetValue(ref distanceZ, value);
+            }
+        }
+        public bool IsFocusZMove
+        {
+            get => isFocusZMove;
+            set
+            {
+                SetValue(ref isFocusZMove, value);
+            }
+        }
+        public int FocusZ
+        {
+            get => focusZ;
+            set
+            {
+                Microscope.ZMoveToCommand(value).Wait();
+                SetValue(ref focusZ, value);
+            }
+        }
+        public int IntensityValue
+        {
+            get => intensityValue;
+            set => SetValue(ref intensityValue, value);
+        }
+        public int ApertureValue
+        {
+            get => apertureValue;
+            set => SetValue(ref apertureValue, value);
+        }
+        private bool isAF;
+        public bool IsAF
+        {
+            get => isAF;
+            set
+            {
+                if (value == true)
+                {
+                    Microscope.AF_Off().Wait();
+                }
+                else
+                {
+                    Microscope.AF_Trace().Wait();
+                }
+                SetValue(ref isAF, value);
+            }
+        }
+
+
+        private bool isLens1;
+        public bool IsLens1
+        {
+            get => isLens1;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeLens(1).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isLens1, value);
+            }
+        }
+
+        private bool isLens2;
+        public bool IsLens2
+        {
+            get => isLens2;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeLens(2).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isLens2, value);
+            }
+        }
+
+        private bool isLens3;
+        public bool IsLens3
+        {
+            get => isLens3;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeLens(3).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isLens3, value);
+            }
+        }
+
+        private bool isLens4;
+        public bool IsLens4
+        {
+            get => isLens4;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeLens(4).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isLens4, value);
+            }
+        }
+
+        private bool isLens5;
+        public bool IsLens5
+        {
+            get => isLens5;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeLens(5).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isLens5, value);
+            }
+        }
+
+
+        private bool isObservation1;
+        public bool IsObservation1
+        {
+            get => isObservation1;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeCube(2).Wait();
+                        Microscope.ChangeFilter(1, 1).Wait();
+                        Microscope.ChangeFilter(2, 1).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isObservation1, value);
+            }
+        }
+
+        private bool isObservation2;
+        public bool IsObservation2
+        {
+            get => isObservation2;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeCube(1).Wait();
+                        Microscope.ChangeFilter(1, 1).Wait();
+                        Microscope.ChangeFilter(2, 1).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isObservation2, value);
+            }
+        }
+        private bool isObservation3;
+        public bool IsObservation3
+        {
+            get => isObservation3;
+            set
+            {
+                if (value == true)
+                {
+                    try
+                    {
+                        Microscope.ChangeFilter(1, 2).Wait();
+                        Microscope.ChangeFilter(2, 2).Wait();
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                SetValue(ref isObservation3, value);
+            }
+        }
+
+
+
+        public ICommand ApertureChange => new RelayCommand<string>(async key =>
+        {
+            try
+            {
+                switch (key)
+                {
+                    case "1":
+                        Microscope.ApertureValue = 100;
+                        break;
+                    case "2":
+                        await Microscope.ChangeAperture(700);
+                        Microscope.ApertureValue = 700;
+                        break;
+                    case "3":
+                        await Microscope.ChangeAperture(1300);
+                        Microscope.ApertureValue = 1300;
+                        break;
+                    case "4":
+                        await Microscope.ChangeAperture(1900);
+                        Microscope.ApertureValue = 1900;
+                        break;
+                    case "5":
+                        await Microscope.ChangeAperture(2500);
+                        Microscope.ApertureValue = 2500;
+                        break;
+                    case "6":
+                        await Microscope.ChangeAperture(3113);
+                        Microscope.ApertureValue = 3113;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        });
+        public ICommand LensChange => new RelayCommand<string>(async key =>
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        });
+        public ICommand FocusZMove => new RelayCommand<string>(async key =>
+        {
+            try
+            {
+                IsFocusZMove = false;
+                switch (key)
+                {
+                    case "Up":
+                        await Microscope.ZMoveCommand(-DistanceZ);
+                        break;
+                    case "Down":
+                        await Microscope.ZMoveCommand(DistanceZ);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                IsFocusZMove = true;
+            }
+        });
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -52,6 +428,42 @@ namespace YuanliCore.Model.Microscope
             OnPropertyChanged(propertyName, oldValue, value);
         }
 
+        protected virtual void OnPropertyChanged<T>(string name, T oldValue, T newValue)
+        {
+            // oldValue 和 newValue 目前沒有用到，代爾後需要再實作。
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    public class BXFMUI : INotifyPropertyChanged
+    {
+        private int focusZ;
+        public int FocusZ
+        {
+            get => focusZ;
+            set => SetValue(ref focusZ, value);
+        }
+        private int lightValue;
+        public int LightValue
+        {
+            get => lightValue;
+            set => SetValue(ref lightValue, value);
+        }
+
+        private int apertureValue;
+        public int ApertureValue
+        {
+            get => apertureValue;
+            set => SetValue(ref apertureValue, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            T oldValue = field;
+            field = value;
+            OnPropertyChanged(propertyName, oldValue, value);
+        }
         protected virtual void OnPropertyChanged<T>(string name, T oldValue, T newValue)
         {
             // oldValue 和 newValue 目前沒有用到，代爾後需要再實作。
