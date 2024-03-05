340d9e953abc7ca4544bf72d8fdc736d02c0ed01
fred_liu <fred_liu@yuanyu.tw>
2024-03-05 13:21:47 +0800

    新增  設備用軟體基礎元件  例:設備狀態

---
+++ YuanliCore.Model/Machine.Base/MachineStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuanliCore.Machine.Base
+{
+    public enum MachineStates
+    {
+        IDLE,
+        RUNNING,
+        PAUSED,//對位異常
+        Alarm,//(紅色 紅燈) 真空異常 上船失敗 等...
+        Emergency//(紅色 紅燈)緊急停止 、軸卡異常 不能往下做，需重開設備
+    }
+}
