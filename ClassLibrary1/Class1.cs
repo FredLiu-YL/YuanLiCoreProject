6d1c4c3c427bb0d630654611693b70c7adcb1db9
fred_liu <fred_liu@yuanyu.tw>
2024-03-21 13:16:05 +0800

    新增專案測試 COGNEX  9.20版

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿//using Cognex.VisionPro.PMAlign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+
+        public Class1()
+        {
+            // CogPMAlignTool  cogPMAlignTool;
+           //  cogPMAlignTool = new CogPMAlignTool();
+        }
+
+    }
+}
